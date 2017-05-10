4fd88069450f24287087310debaab146cc63d6bf
mark woodrow <markwoodrow@yahoo.co.uk>
2017-05-10 22:15:08 +0100

    weds pm at home

---
+++ simple files.c
@@ -0,0 +1,17 @@
+/**
+ * simple files
+ */
+
+
+/**
+ * the two modes, text and binary
+ * 
+ * text is based on printf, uses fprintf, so we already know it
+ * FILE* is a struct ptr
+ * 
+ * Need for i/o error checking as never before
+ 
+ * fputs, fgets
+ * fprintf, fscanf
+ * fputc, fgetc
+ */
