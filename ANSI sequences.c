c9082b2e496620cd378d17603ad064806edbe0d4
mark woodrow <markwoodrow@yahoo.co.uk>
2017-05-18 19:36:48 +0100

    Added ANSI demo code

--- ANSI sequences.c
+++ ANSI sequences.c
@@ -6,9 +6,40 @@
 
 void ansiDemo() {
 
-    printf("%c%c;%cm\n", 27,31,43);
-    printf("Hello World!\n");
 
-    
+    // clear screen
+    printf("%c[2J", 27);
+    // 10 forward
+    //printf("%c[10C", 27);
+    // 
+    /* Foreground
+    30	Black
+    31	Red
+    32	Green
+    33	Yellow
+    34	Blue
+    35	Magenta
+    36	Cyan
+    37	White
+
+    Background
+    40	Black
+    41	Red
+    42	Green
+    43	Yellow
+    44	Blue
+    45	Magenta
+    46	Cyan
+    47	White
+     */
+
+    int fore, back;
+
+    for (back = 40; back <= 47; back++) {
+        for (fore = 30; fore <= 37; fore++) {
+            printf("%c[%2d;%2dm  hello", 27, fore, back);
+        }
+        printf("\n");
+    }
 
 }
