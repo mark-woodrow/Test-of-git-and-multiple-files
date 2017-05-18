909a2cda9f67b8212cb980053faa3eed244e8083
mark-woodrow <markwoodrow@yahoo.co.uk>
2017-05-18 19:18:50 +0100

    Thursday pm from laptop

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/ANSI\ sequences.o \
 	${OBJECTDIR}/argc\ and\ argv.o \
 	${OBJECTDIR}/main.o \
 	${OBJECTDIR}/simple\ files.o \
@@ -66,6 +67,12 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/test-of-git-and-multiple-files: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/test-of-git-and-multiple-files ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+.NO_PARALLEL:${OBJECTDIR}/ANSI\ sequences.o
+${OBJECTDIR}/ANSI\ sequences.o: ANSI\ sequences.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ANSI\ sequences.o ANSI\ sequences.c
 
 .NO_PARALLEL:${OBJECTDIR}/argc\ and\ argv.o
 ${OBJECTDIR}/argc\ and\ argv.o: argc\ and\ argv.c 
