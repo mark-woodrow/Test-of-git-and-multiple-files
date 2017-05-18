d21582690edde5713da90fa2b78d5356dfc9a564
mark-woodrow <markwoodrow@yahoo.co.uk>
2017-05-18 19:30:05 +0100

    Thursday pm from laptop

---
+++ ANSI sequences.c
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+void ansiDemo() {
+
+    printf("%c%c;%cm\n", 27,31,43);
+    printf("Hello World!\n");
+
+    
+
+}
